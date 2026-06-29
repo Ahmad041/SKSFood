fd172e9c2e97762276d58925754a5151b6013fa1
nirmalanurul <nirmalanurul02@gmail.com>
2026-06-29 18:54:37 +0700

    fxml of login

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,11 @@ module org.gui.sksfood {
 
     requires com.dlsc.formsfx;
     requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
 
+    // Package utama (MainApp)
     opens org.gui.sksfood to javafx.fxml;
     exports org.gui.sksfood;
+
+    opens org.gui.sksfood.controller to javafx.fxml;
 }
